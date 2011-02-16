2e4ed13b33bbe4ebee0abff834b023a4be72dd3b
Paul J. Davis <paul.joseph.davis@gmail.com>
2011-02-16 15:40:16 -0500

    Remove data and code from the original pystache.
    New pystache has been rewritten in the style of the newer Ruby
    version that uses a parser/tokenizer and a formatter to generate
    code dynamically.

--- old/pystache/__init__.py
+++
@@ -1,9 +0,0 @@
-from pystache.template import Template
-from pystache.view import View
-
-def render(template, context=None, **kwargs):
-    if context is None:
-        context = {}
-    else:
-        context = context.copy()
-    return Template(template, context).render()
